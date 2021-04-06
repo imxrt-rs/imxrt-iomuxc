2cf0143d05c7bebb977b4255e3950a116c7a6f74
Ian McIntyre <ianpmcintyre@gmail.com>
2021-04-05 20:35:33 -0400

    adc: favor typenum identifiers for ADC instances
    This should match with the upcoming imxrt-ral changes.

--- src/adc.rs
+++ src/adc.rs
@@ -1,28 +1,18 @@
 //! ADC pad configuration
 
-/// An ADC input bank, like `ADC1` and `ADC2`
-pub trait ADC: private::Sealed {}
+use crate::consts::Unsigned;
 
-mod private {
-    pub trait Sealed {}
-    impl Sealed for super::ADC1 {}
-    impl Sealed for super::ADC2 {}
-}
-
-/// Indicates an ADC1-compatible pin
-pub enum ADC1 {}
-impl ADC for ADC1 {}
-
-/// Indicates an ADC2-compatible pin
-pub enum ADC2 {}
-impl ADC for ADC2 {}
+/// Type number for ADC1
+pub type ADC1 = crate::consts::U1;
+/// Type number for ADC2
+pub type ADC2 = crate::consts::U2;
 
 /// Describes an ADC input pin
 ///
 /// ADC pins are specialized GPIO pins. Some pads may be used in both `ADC1`
 /// and `ADC2`, so implementations will indicate their compatibility by
 /// supplying an identifier in place of `ADCx`.
-pub trait Pin<ADCx: ADC>: super::gpio::Pin {
+pub trait Pin<U: Unsigned>: super::gpio::Pin {
     /// The input pin identifier
     ///
     /// Starts at `U0`, and increments up.
@@ -33,7 +23,7 @@ pub trait Pin<ADCx: ADC>: super::gpio::Pin {
 ///
 /// Due to a requirement in the ADC module, `prepare` will disable the pull/keeper
 /// on the pin. The configuration change will not affect any other settings.
-pub fn prepare<ADCx: ADC, P: Pin<ADCx>>(pin: &mut P) {
+pub fn prepare<U: Unsigned, P: Pin<U>>(pin: &mut P) {
     // See the note in the ADC section of the reference manual
     // (using iMXRT1060, rev 2). ADC input signals connect to
     // GPIO, and we need to disable the keeper to prevent signal
