21bee046f71d03ec057d1dce1fe4a85241cd7c91
Ian McIntyre <ianpmcintyre@gmail.com>
2022-10-31 10:16:36 -0400

    Add CCM clock out pins for 1170

--- src/imxrt1170/mod.rs
+++ src/imxrt1170/mod.rs
@@ -4,3 +4,16 @@ mod pads;
 pub use pads::*;
 
 mod lpuart;
+
+mod ccm {
+    pub use crate::ccm::{Function, Observable, Pin};
+
+    impl Pin for super::pads::gpio_emc_b1::GPIO_EMC_B1_40 {
+        const ALT: u32 = 9;
+        type Function = Observable<1>;
+    }
+    impl Pin for super::pads::gpio_emc_b1::GPIO_EMC_B1_41 {
+        const ALT: u32 = 9;
+        type Function = Observable<2>;
+    }
+}
