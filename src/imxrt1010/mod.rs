b2413c054e26edaa36aba70d0d4521da7640d2b6
Florian Zeitz <florob@babelmonkeys.de>
2023-09-10 00:32:32 +0200

    Add 1010 and 1020 FlexIO implementations

--- src/imxrt1010/mod.rs
+++ src/imxrt1010/mod.rs
@@ -3,6 +3,7 @@
 //! The module exports all of the i.MX RT 1010 processor's pads. Pads that can support peripheral
 //! functions are tagged with `imxrt-iomuxc` traits.
 
+mod flexio;
 mod flexpwm;
 mod lpi2c;
 mod lpspi;
