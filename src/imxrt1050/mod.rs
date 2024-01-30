3242b8bdd52a8cbb84f1b184b354fa817f9117d1
Hatim Thayyil <hatim@thayyil.net>
2024-01-30 14:40:39 +0000

    add adc, lpspi, lpuart from 1060

--- src/imxrt1050/mod.rs
+++ src/imxrt1050/mod.rs
@@ -3,5 +3,9 @@
 //! The module exports all of the i.MX RT 1050 processor's pads. Pads that can support peripheral
 //! functions are tagged with `imxrt-iomuxc` traits.
 
+mod adc;
+mod lpspi;
+mod lpuart;
+
 mod pads;
 pub use pads::*;
